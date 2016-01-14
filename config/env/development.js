f048c86c773b18dc7c39384dc180a6a455395bbe
Brendan Lietz <brendan.lietz@gmail.com>
2016-01-15 01:38:53 +1100

    Day2 intro to Grids

--- config/env/development.js
+++ config/env/development.js
@@ -4,10 +4,10 @@ var defaultEnvConfig = require('./default');
 
 module.exports = {
   db: {
-    uri: process.env.MONGOHQ_URL || process.env.MONGOLAB_URI || 'mongodb://' + (process.env.DB_1_PORT_27017_TCP_ADDR || 'localhost') + '/mean-dev',
+    uri: process.env.MONGOHQ_URL || process.env.MONGOLAB_URI || 'mongodb://<dbuser>:<dbpassword>@ds045465.mongolab.com:45465/myfirstapp-db',
     options: {
-      user: '',
-      pass: ''
+      user: 'MongoDBAdmin',
+      pass: '!@12qwaszx'
     },
     // Enable mongoose debug mode
     debug: process.env.MONGODB_DEBUG || false
